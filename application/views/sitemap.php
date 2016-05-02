17c72d1144542e4cd287fb0607e30466a9820f17
Varun Shrivastava <Varun Shrivastava>
2016-05-03 00:35:43 +0530

    changed sitemap.xml :clap:

--- application/views/sitemap.php
+++ application/views/sitemap.php
@@ -9,7 +9,7 @@
     <!-- My code is looking quite different, but the principle is similar -->
     <?php foreach($data as $row) { ?>
     <url>
-        <loc><?= base_url().'site/blog/'.$row['id'].'/'.url_title($row['heading']); ?></loc>
+        <loc><?= base_url().url_title($row['heading']); ?></loc>
         <priority>0.5</priority>
     </url>
     <?php } ?>
